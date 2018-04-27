8428dba33c6712e6fe625e2b64ee43198e396606
noeliaescalier <noesc@est.frba.utn.edu.ar>
2018-04-27 00:05:26 -0300

    Modelado de tipo de dato

--- MicroEntrega1.hs
+++ MicroEntrega1.hs
@@ -1,1 +1,17 @@
-probando
+module MicroEntrega where
+
+import Text.Show.Functions
+
+-- Modelado de tipo de dato
+
+data Microcontrolador = Microcontrolador{
+	nombre :: String,
+	memoriaDeDatos :: [Int],
+	acumuladorA :: Int ,
+	acumuladorB :: Int,
+	programCounter :: Int ,
+	etiqueta :: String,
+	instrucciones :: [Instruccion]
+}deriving (Show)
+
+type Instruccion = Microcontrolador -> Microcontrolador
